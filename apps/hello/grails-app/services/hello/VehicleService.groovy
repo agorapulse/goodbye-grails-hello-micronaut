211d7db15a00e16bda3e6cc711f274192c3285aa
Vladimir Orany <vladimir@orany.cz>
2021-07-30 12:21:10 +0200

    7) Services - before

---
+++ apps/hello/grails-app/services/hello/VehicleService.groovy
@@ -0,0 +1,13 @@
+package hello
+
+import groovy.transform.CompileStatic
+import hello.legacy.VehicleDataService
+
+@CompileStatic
+class VehicleService {
+
+    VehicleDataService vehicleDataService
+
+    // methods
+
+}
